5377eab852fff370c59cb9239d474bde280cf327
Finn <github@finnsweb.de>
2020-03-23 16:53:03 +0100

    Removed log

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ const hostname = env === 'dev' ? 'dev.studicar.mfinn.de' : 'studicar.mfinn.de';
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+http.createServer(options, (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+}).listen(port, hostname);
