86b9b3f26350cc59ead8aa020cf7abd12e7b772e
Finn <github@finnsweb.de>
2020-03-26 19:38:20 +0100

    Test

--- API/index.js
+++ API/index.js
@@ -3,7 +3,6 @@ if (typeof (PhusionPassenger) !== 'undefined') {
 }
 
 var http = require('http');
-
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("Hello World");
