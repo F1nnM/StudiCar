0c8cd0933fd1771cdce0f1afc8006ed5862ce53b
Finn <github@finnsweb.de>
2020-04-06 17:08:13 +0200

    Reinstalled Quasar

--- pwa/src/pages/marketplace.vue
+++ pwa/src/pages/marketplace.vue
@@ -1,36 +1,29 @@
 <template>
   <div class="q-pa-md">
     <div class="q-gutter-y-md" style="max-width: 600px">
-            <div class="text-h4">{{ greeting }}, {{ user.name }}</div>
-            <hr>
-            <br>
-            
-
+      <div class="text-h4">{{ greeting }}, {{ user.name }}</div>
+      <hr />
+      <br />
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-
-data(){
-    var hours = (new Date()).getHours();
-    var greeting = 'Willkommen';
-    if(hours > 18){
-        greeting = 'Guten Abend';
-    }
-    else if(hours < 8){
-        greeting = 'Guten Morgen';
+  data() {
+    var hours = new Date().getHours();
+    var greeting = "Willkommen";
+    if (hours > 18) {
+      greeting = "Guten Abend";
+    } else if (hours < 8) {
+      greeting = "Guten Morgen";
     }
     return {
-        'greeting': greeting,
-        user: {
-            name: 'Bernd'
-        }
-    }
-}
-    
-    
-}
+      greeting: greeting,
+      user: {
+        name: "Bernd"
+      }
+    };
+  }
+};
 </script>
