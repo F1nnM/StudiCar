ebeaf60c6b3097ecf5e1d63e143f4bbd9b8b3e45
Finn <github@finnsweb.de>
2020-04-16 18:40:53 +0200

    Added basic api

--- pwa/src/pages/playground.vue
+++ pwa/src/pages/playground.vue
@@ -1,21 +1,46 @@
 <template>
   <div class="q-pa-md">
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem qui sequi voluptates perspiciatis tempore obcaecati hic, esse ipsum corrupti corporis molestiae debitis porro omnis, sapiente non delectus placeat ipsa cupiditate.</p>
+    <pre>{{sqlTestData}}</pre>
+    <input v-model="field"><br>
+    <button @click="writeTestData();">Add Data</button>
+    <button @click="getTestData();">Load Data</button>
   </div>
 </template>
 
 <script>
-
+import { sendApiRequest, SQL_TEST_READ, SQL_TEST_ADD } from "../ApiAccess";
 export default {
-  
-  data () {
+  data() {
     return {
-      chats: 'play',
+      chats: "play",
       logged: false,
       scrolled: false,
-      value: 'Hallo'
+      value: "Hallo",
+      sqlTestData: "",
+      field: ""
+    };
+  },
+  methods: {
+    getTestData() {
+      sendApiRequest(
+        SQL_TEST_READ,
+        null,
+        data => (this.sqlTestData += JSON.stringify(data) + "\n"),
+        error => (this.sqlTestData += "ERROR: " + JSON.stringify(error) + "\n")
+      );
+    },
+    writeTestData() {
+      sendApiRequest(
+        SQL_TEST_ADD,
+        { data: this.field },
+        data => (this.sqlTestData += JSON.stringify(data) + "\n"),
+        error => (this.sqlTestData += "ERROR: " + JSON.stringify(error) + "\n")
+      );
     }
+  },
+  mounted() {
+    this.getTestData();
   }
-}
-
+};
 </script>
