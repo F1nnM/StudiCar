d37aadf8f9f16aeeb5169967057100d17b6cb0cc
Finn <github@finnsweb.de>
2020-04-16 18:43:46 +0200

    Test to check deployment

--- pwa/src/pages/playground.vue
+++ pwa/src/pages/playground.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="q-pa-md">
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem qui sequi voluptates perspiciatis tempore obcaecati hic, esse ipsum corrupti corporis molestiae debitis porro omnis, sapiente non delectus placeat ipsa cupiditate.</p>
+    <p>TESTESTESTSETLorem ipsum dolor, sit amet consectetur adipisicing elit. Autem qui sequi voluptates perspiciatis tempore obcaecati hic, esse ipsum corrupti corporis molestiae debitis porro omnis, sapiente non delectus placeat ipsa cupiditate.</p>
     <pre>{{sqlTestData}}</pre>
     <input v-model="field"><br>
     <button @click="writeTestData();">Add Data</button>
