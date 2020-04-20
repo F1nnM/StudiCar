d1e488008f4ce6f6909d942a979ab92e4d4fe9d3
Finn <github@finnsweb.de>
2020-04-20 19:41:44 +0200

    HTTPS

--- api/api.js
+++ api/api.js
@@ -1,5 +1,16 @@
 var runQuery = require('./db');
 
+function isOptionMissing(data, needed, res) {
+  return needed.some(key => {
+    if (typeof data[key] == "undefined") {
+      res.writeHead(400);
+      res.end("No field '" + key + "' supplied!");
+      return true;
+    }
+    return false;
+  });
+}
+
 module.exports = {
   'GET': {
     '/ping': (req, res, options) => {
@@ -12,20 +23,16 @@ module.exports = {
   },
   'POST': {
     '/sqlTest': async (req, res, options) => {
-      if (!options.data) {
-        res.writeHead(400);
-        res.end("No field 'data' supplied!");
+      if (!isOptionMissing(options, ['data'], res)) {
+        let result = await runQuery("INSERT INTO `TEST` (`ID`, `data`) VALUES (NULL, ?)", options.data);
+        res.write(JSON.stringify(result))
       }
-      let result = await runQuery("INSERT INTO `TEST` (`ID`, `data`) VALUES (NULL, ?)", options.data);
-      res.write(JSON.stringify(result))
     },
     '/addUser': async (req, res, options) => {
-      if (!options.data) {
-        res.writeHead(400);
-        res.end("No field 'data' supplied!");
+      if (!isOptionMissing(options, ['FbId', 'Name', 'Gender', 'Course', ], res)) {
+        let result = await runQuery("INSERT INTO `TEST` (`ID`, `data`) VALUES (NULL, ?)", options.data || '');
+        res.write(JSON.stringify(result))
       }
-      let result = await runQuery("INSERT INTO `TEST` (`ID`, `data`) VALUES (NULL, ?)", options.data || '');
-      res.write(JSON.stringify(result))
     }
   }
 }
