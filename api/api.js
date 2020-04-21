94d5eb4214aff506e835caa131bf3de3b2ec71ff
Finn <github@finnsweb.de>
2020-04-21 13:10:50 +0200

    Minor fixes in POST-Request

--- api/api.js
+++ api/api.js
@@ -14,30 +14,30 @@ function isOptionMissing(data, needed, res) {
 module.exports = {
   'GET': {
     '/ping': (req, res, options) => {
-      res.write('pong');
+      res.end('pong');
     },
     '/sqlTest': async (req, res, options) => {
       let result = await runQuery("SELECT * FROM `TEST`");
-      res.write(JSON.stringify(result));
+      res.end(JSON.stringify(result));
     }
   },
   'POST': {
     '/sqlTest': async (req, res, options) => {
       if (!isOptionMissing(options, ['data'], res)) {
         let result = await runQuery("INSERT INTO `TEST` (`ID`, `data`) VALUES (NULL, ?)", options.data);
-        res.write(JSON.stringify(result))
+        res.end(JSON.stringify(result))
       }
     },
     '/addUser': async (req, res, options) => {
       if (!isOptionMissing(options, ['fbid', 'name', 'gender', 'mail'], res)) {
         let size = 200
         let png = jdenticon.toPng(options.name, size);
-
+        
         let result = await runQuery(
           "INSERT INTO `USER` (`ID`, `FB_ID`, `NAME`, `GENDER`, `COURSE`, `PICTURE`, `DESCRIPTION`, `CREATED_DATE`, `MAIL`, `PREF_SMOKE`, `PREF_MUSIC`, `PREF_TALK`, `PREF_TALK_MORNING`)" +
           "VALUES (NULL, ?, ?, ?, '', ?, '', NULL, ?, 'RED', 'RED', 'RED', 'RED')",
           options.FbId, options.name, options.gender, png, options.mail);
-        res.write(JSON.stringify(result))
+        res.end(JSON.stringify(result))
       }
     }
   }
