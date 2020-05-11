f0621a1e74e8d7e785957dc1460c8b2f1e965d9f
Finn <github@finnsweb.de>
2020-05-11 11:07:16 +0200

    Added offers to marketplace

---
+++ pwa/src/components/LiftOffer.vue
@@ -0,0 +1,121 @@
+<template>
+  <q-card class="row items-stretch">
+    <div id="map" class="col-grow row"></div>
+    <div class="col column q-pa-sm">
+      <div class="row">
+        <div class="col">
+          <span>{{lift.city}} {{lift.distance}}km</span>
+        </div>
+        <div class="col-auto">
+          <span>{{lift.seats_occupied}} / {{lift.seats_offered}}</span>
+          <q-icon id="seats" name="airline_seat_recline_normal" />
+        </div>
+      </div>
+      <div v-if="lift.day_pattern" class="row">
+        <div
+          v-for="(values, day) in lift.day_pattern"
+          :key="day"
+          class="drive_indicator"
+          :class="'drive_indicator_'+(values[0]+values[1])"
+          :title="day"
+        >
+          <q-tooltip anchor="top middle" self="bottom middle">{{day}}</q-tooltip>
+          <q-icon name="unfold_more" v-if="values[0] && values[1]" />
+          <q-icon name="arrow_upward" v-else-if="values[0]" />
+          <q-icon name="arrow_downward" v-else-if="values[1]" />
+          <q-icon name="close" v-else />
+        </div>
+      </div>
+      <div v-else-if="date_single_drive[0]==date_single_drive[1]" class="row">
+        <div class="drive_indicator drive_indicator_2">
+          <q-icon name="unfold_more" />
+        </div>
+        <span class="vertical_middle">{{date_single_drive[0]}}</span>
+      </div>
+      <div v-else class="row">
+        <div class="drive_indicator drive_indicator_1" v-if="date_single_drive[0]">
+          <q-icon name="arrow_upward" />
+        </div>
+        <span class="vertical_middle" v-if="date_single_drive[0]">{{date_single_drive[0]}}</span>
+        <div class="drive_indicator drive_indicator_1" v-if="date_single_drive[1]">
+          <q-icon name="arrow_downward" />
+        </div>
+        <span class="vertical_middle" v-if="date_single_drive[1]">{{date_single_drive[1]}}</span>
+      </div>
+      <div class="row self-end">
+        <span>
+          Angeboten von
+          <a v-bind:href="'#/view-user?'+lift.driver.fbid">{{lift.driver.name}}</a>
+        </span>
+      </div>
+    </div>
+  </q-card>
+</template>
+
+<script>
+import { date } from 'quasar'
+export default {
+  name: 'LiftOffer',
+  props: ['lift'],
+  data() {
+    return {
+    }
+  },
+  computed: {
+    date_single_drive() {
+      if (!this.lift)
+        return ""
+      if (!this.lift.next_drive)
+        return ""
+      return [
+        this.lift.next_drive[0] ? date.formatDate(this.lift.next_drive[0], 'DD. MM. YY') : null,
+        this.lift.next_drive[1] ? date.formatDate(this.lift.next_drive[1], 'DD. MM. YY') : null
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#map {
+  max-width: 200px;
+  flex-basis: 15vw;
+  overflow: hidden;
+  position: relative;
+  background-image: url("~assets/loremmap.png");
+  background-size: cover;
+}
+#mapImg {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 50%;
+  /*max-width: 100%;*/
+  min-height: 100%;
+}
+.drive_indicator {
+  width: 30px;
+  height: 30px;
+  margin: 4px;
+  border-radius: 100%;
+  background-color: #888;
+  text-align: center;
+  line-height: 30px;
+  font-size: larger;
+}
+.drive_indicator_0 {
+  background-color: #ff000080;
+}
+.drive_indicator_1 {
+  background-color: #ffa60080;
+}
+.drive_indicator_2 {
+  background-color: #00ff0080;
+}
+#seats {
+  font-size: larger;
+}
+.vertical_middle {
+  line-height: 38px;
+}
+</style>
--- pwa/src/pages/marketplace.vue
+++ pwa/src/pages/marketplace.vue
@@ -2,18 +2,69 @@
   <div class="q-pa-md">
     <div class="q-gutter-y-md">
       <p>Der Marktplatz ist so etwas wie das Schwarze Brett von StudiCar. Hier siehst du alle aktuellen Mitfahrgelegenheiten, aufsteigend nach Entfernung geordnet.</p>
-      <p v-for="n in 20" :key="n">Lorem</p>
+      <LiftOffer v-for="lift in lifts" :key="lift.index" v-bind:lift="lift">{{lift.city}}</LiftOffer>
     </div>
   </div>
 </template>
 
 <script>
+import LiftOffer from '../components/LiftOffer';
 export default {
-
-
+  components: {LiftOffer},
   data() {
     return {
-      
+      lifts: [
+        {
+          id: 1,
+          driver: {
+            fbid: "QTs2vuk6O0RHjr8uDyLBwb9DZ5G3",
+            name: "Finn"
+          },
+          day_pattern: null,
+          next_drive: [new Date(), new Date()],
+          postcode: 70182,
+          city: "Stuttgart",
+          distance: 20,
+          seats_offered: 4,
+          seats_occupied: 2
+        },
+        {
+          id: 2,
+          driver: {
+            fbid: "QTs2vuk6O0RHjr8uDyLBwb9DZ5G3",
+            name: "Finn"
+          },
+          day_pattern: null,
+          next_drive: [new Date("2-2-2020"), new Date()],
+          postcode: 70182,
+          city: "Stuttgart",
+          distance: 20,
+          seats_offered: 3,
+          seats_occupied: 1
+        },
+        {
+          id: 3,
+          driver: {
+            fbid: "QTs2vuk6O0RHjr8uDyLBwb9DZ5G3",
+            name: "Finn"
+          },
+          day_pattern: {
+            Montag: [true, true],
+            Dienstag: [false, true],
+            Mittwoch: [true, false],
+            Donnerstag: [true, true],
+            Freitag: [false, true],
+            Samstag: [false, false],
+            Sonntag: [false, false]
+          },
+          next_drive: null,
+          postcode: 70182,
+          city: "Stuttgart",
+          distance: 20,
+          seats_offered: 2,
+          seats_occupied: 0
+        },
+      ]
     };
   },
 
