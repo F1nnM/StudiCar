2ce782b02e65b31ea236c74e8be822a1d727006d
unknown <berndmayinger@gmail.com>
2020-05-21 22:14:15 +0200

    set primary color to logo color set theme color to logo color beautiful loading screen

--- pwa/src/layouts/without_nav.vue
+++ pwa/src/layouts/without_nav.vue
@@ -1,15 +1,30 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
+      <q-toolbar v-if="$store.getters['auth/signinLoaded']">
         <q-toolbar-title>StudiCar Authentifizierung</q-toolbar-title>
       </q-toolbar>
     </q-header>
-    <q-page-container >
-      <div class="overlay" :class="$store.getters['auth/signinLoaded']?'transparent':''">
-        <q-circular-progress indeterminate size="50px" class="q-ma-md center" />
+    <q-page-container>
+      <div class="fixed-top text-center q-mt-xl q-pt-xl">
+        <p class="text-h4">
+          <span class="text-primary">Studi</span>Car
+        </p>
+        <p>Green. Cheap. Social.</p>
       </div>
-      <router-view v-if="$store.getters['auth/signinLoaded']"/>
+      <div
+        class="fixed-center full-width q-px-xl z-top"
+        :class="$store.getters['auth/signinLoaded']?'transparent':''"
+      >
+        <div class="q-mt-xl">
+          <p class="col-9 q-ma-none">Bitte hab noch einen Moment Geduld</p>
+        </div>
+        <q-linear-progress indeterminate rounded color="primary" class="q-mt-sm" />
+      </div>
+      <div class="fixed-bottom overflow-hidden-y q-mb-lg q-mx-xl text-center">
+        <div class="branding-rise">Mit freundlicher Unterstützung von</div>
+      </div>
+      <router-view v-if="$store.getters['auth/signinLoaded']" />
     </q-page-container>
   </q-layout>
 </template>
@@ -24,12 +39,6 @@ export default {
 </script>
 
 <style scoped>
-.center {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
 .overlay {
   position: absolute;
   top: 0;
@@ -43,5 +52,17 @@ export default {
 .transparent {
   opacity: 0;
   visibility: hidden;
+}
+
+.branding-rise {
+  animation: rise 0.4s forwards;
+  animation-delay: 0.3s;
+  transform: translateY(-100%);
+
+  @keyframes rise {
+    to {
+      transform: translateY(0);
+    }
+  }
 }
 </style>
