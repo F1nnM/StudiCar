ae8e4dc499b86344b24ac11c7c9186b47b86a53a
Bernd Mayinger <berndmayinger@gmail.com>
2021-01-12 20:32:14 +0100

    new: redesigned layout of sorting and filtering, logic not implemented yet

--- pwa/src/pages/marketplace.vue
+++ pwa/src/pages/marketplace.vue
@@ -1,45 +1,213 @@
 <template>
   <div class="q-pa-md">
-    <div class="q-gutter-y-md">
-      <div class="absolute-top-right q-mt-xl q-mr-xl q-pt-md">
-        <q-btn dense flat color="primary" @click="$refs.select.showPopup()" icon="sort">
-          <q-icon size="xs" :name="sort.icon" />
-        </q-btn>
-      </div>
-
-      <div :key="offerIndexToRefresh">
-        <q-select
-          v-model="sort"
-          ref="select"
-          options-selected-class="border-right border-right-green"
-          rounded
-          transition-show="jump-up"
-          transition-hide="jump-down"
-          outlined
-          map-options
-          class="hidden"
-          :style="'border-radius: ' + (sort ? 0 : 100) + 'px'"
-          label="Angebote sortieren"
-          :options="sortOptions"
+    <TitleButton>
+      <q-btn dense flat @click="openSortFilterDialog('sort')" icon="sort">
+        <q-icon size="xs" :name="sort.icon" />
+      </q-btn>
+      <span class="q-mx-xs">|</span>
+      <q-btn color="primary" dense flat @click="openSortFilterDialog('filter')">
+        <!-- filter icon, didn't work the normal way -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          enable-background="new 0 0 24 24"
+          viewBox="0 0 24 24"
+          fill="black"
+          width="18px"
+          height="18px"
         >
-          <template v-slot:prepend>
-            <q-icon name="sort" />
-          </template>
-          <template v-if="sort" v-slot:append>
-            <q-icon name="cancel" @click.stop="sort = null" class="cursor-pointer" />
-          </template>
-          <template v-slot:option="scope">
-            <q-item v-bind="scope.itemProps" v-on="scope.itemEvents" :disable="scope.opt.disabled">
-              <q-item-section avatar>
-                <q-icon :name="scope.opt.icon" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label v-html="scope.opt.label" />
-                <q-item-label caption>{{ scope.opt.caption }}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </template>
-        </q-select>
+          <g>
+            <path d="M0,0h24 M24,24H0" fill="none" />
+            <path
+              d="M4.25,5.61C6.27,8.2,10,13,10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-6c0,0,3.72-4.8,5.74-7.39 C20.25,4.95,19.78,4,18.95,4H5.04C4.21,4,3.74,4.95,4.25,5.61z"
+            />
+            <path d="M0,0h24v24H0V0z" fill="none" />
+          </g>
+        </svg>
+        <span style="margin-left: 2px" v-html="filtersActive" v-if="filtersActive" />
+      </q-btn>
+    </TitleButton>
+    <div class="q-gutter-y-md">
+      <div :key="offerIndexToRefresh">
+        <q-dialog
+          position="right"
+          v-model="sortFilterDialog.open"
+          transition-show="jump-left"
+          transition-hide="jump-right"
+        >
+          <q-card>
+            <div class="row justify-between">
+              <q-item-label header>{{ sortFilterDialog.tab == 'sort' ? 'Sortieren' : 'Filtern' }}</q-item-label>
+              <q-tabs
+                v-model="sortFilterDialog.tab"
+                narrow-indicator
+                dense
+                class="bg-white text-dark"
+              >
+                <q-tab name="sort" icon="sort" />
+                <q-tab name="filter">
+                  <q-icon size="sm">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      enable-background="new 0 0 24 24"
+                      viewBox="0 0 24 24"
+                      fill="black"
+                      width="18px"
+                      height="18px"
+                    >
+                      <g>
+                        <path d="M0,0h24 M24,24H0" fill="none" />
+                        <path
+                          d="M4.25,5.61C6.27,8.2,10,13,10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-6c0,0,3.72-4.8,5.74-7.39 C20.25,4.95,19.78,4,18.95,4H5.04C4.21,4,3.74,4.95,4.25,5.61z"
+                        />
+                        <path d="M0,0h24v24H0V0z" fill="none" />
+                      </g>
+                    </svg>
+                  </q-icon>
+                </q-tab>
+              </q-tabs>
+            </div>
+            <q-tab-panels
+              v-model="sortFilterDialog.tab"
+              animated
+              transition-next="jump-left"
+              transition-prev="jump-right"
+            >
+              <q-tab-panel name="sort">
+                <q-list>
+                  <q-item
+                    v-for="option in sortOptions"
+                    :key="option.val"
+                    :disable="option.disabled"
+                    tag="label"
+                  >
+                    <q-item-section avatar outlined>
+                      <q-radio
+                        @input="sortFilterDialog.open = false"
+                        class="hidden"
+                        v-model="sort"
+                        :disable="option.disabled"
+                        :val="option"
+                      ></q-radio>
+                      <q-btn
+                        :outline="sort.val == option.val"
+                        :flat="sort.val != option.val"
+                        :color="sort.val == option.val ? 'primary' : 'dark'"
+                        round
+                        @click="keepDialogOpen('sort', option)"
+                        dense
+                      >
+                        <q-icon :name="option.icon" color="dark" />
+                      </q-btn>
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label v-html="option.label" />
+                      <q-item-label caption>{{ option.caption }}</q-item-label>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-tab-panel>
+
+              <q-tab-panel name="filter">
+                <q-list>
+                  <q-item class="q-mb-sm">
+                    <q-item-section avatar>
+                      <q-btn-toggle
+                        v-model="filter.friends"
+                        no-caps
+                        rounded
+                        unelevated
+                        outline
+                        dense
+                        toggle-color="primary"
+                        color="white"
+                        text-color="dark"
+                        :options="[
+          {icon: 'sentiment_satisfied_alt', value: 0},
+          {value: 1, slot: 'groups'},
+          {icon: 'group', value: 2}
+        ]"
+                      >
+                        <template v-slot:groups>
+                          <q-icon>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              enable-background="new 0 0 24 24"
+                              viewBox="0 0 24 24"
+                              :fill="filter.friends == 1 ? 'primary' : 'black'"
+                              width="18px"
+                              height="18px"
+                            >
+                              <rect fill="none" height="24" width="24" />
+                              <g>
+                                <path
+                                  d="M12,12.75c1.63,0,3.07,0.39,4.24,0.9c1.08,0.48,1.76,1.56,1.76,2.73L18,18H6l0-1.61c0-1.18,0.68-2.26,1.76-2.73 C8.93,13.14,10.37,12.75,12,12.75z M4,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C2,12.1,2.9,13,4,13z M5.13,14.1 C4.76,14.04,4.39,14,4,14c-0.99,0-1.93,0.21-2.78,0.58C0.48,14.9,0,15.62,0,16.43V18l4.5,0v-1.61C4.5,15.56,4.73,14.78,5.13,14.1z M20,13c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2s-2,0.9-2,2C18,12.1,18.9,13,20,13z M24,16.43c0-0.81-0.48-1.53-1.22-1.85 C21.93,14.21,20.99,14,20,14c-0.39,0-0.76,0.04-1.13,0.1c0.4,0.68,0.63,1.46,0.63,2.29V18l4.5,0V16.43z M12,6c1.66,0,3,1.34,3,3 c0,1.66-1.34,3-3,3s-3-1.34-3-3C9,7.34,10.34,6,12,6z"
+                                />
+                              </g>
+                            </svg>
+                          </q-icon>
+                        </template>
+                      </q-btn-toggle>
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label>Angebote von Nutzern filtern</q-item-label>
+                      <q-item-label caption>
+                        <span v-if="filter.friends == 2">nur Angebote von Freunden werden angezeigt</span>
+                        <span
+                          v-else-if="filter.friends == 1"
+                        >nur Angebote von Freunden und deren Freunden werden angezeigt</span>
+                        <span v-else-if="!filter.friends">Angebote von jedem Nutzer werden angezeigt</span>
+                      </q-item-label>
+                    </q-item-section>
+                  </q-item>
+                  <q-item class="q-mb-sm">
+                    <q-item-section avatar>
+                      <q-btn-toggle
+                        v-model="filter.dest"
+                        no-caps
+                        rounded
+                        unelevated
+                        outline
+                        dense
+                        toggle-color="primary"
+                        color="white"
+                        text-color="dark"
+                        :options="[
+          {icon: 'home', value: 'home'},
+          {icon: 'sync_alt', value: 0},
+          {icon: 'school', value: 'school'}
+        ]"
+                      />
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label>Fahrtziel filtern</q-item-label>
+                      <q-item-label caption>
+                        <span
+                          v-if="filter.dest == 'home'"
+                        >nur Fahrten in Richtung deiner Standardadresse werden angezeigt</span>
+                        <span
+                          v-else-if="filter.dest == 'school'"
+                        >nur Fahrten in Richtung DH werden angezeigt</span>
+                        <span v-else>es werden alle Fahrten angezeigt</span>
+                      </q-item-label>
+                    </q-item-section>
+                  </q-item>
+                  <q-item tag="label">
+                    <q-item-section avatar>
+                      <q-toggle v-model="filter.prefs" />
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label>Meine Präferenzen berücksichtigen</q-item-label>
+                      <q-item-label caption>
+                        <span v-if="filter.prefs">Im Moment werden deine Präferenzen berücksichtigt</span>
+                        <span v-else>Im Moment werden deine Präferenzen nicht beachtet</span>
+                      </q-item-label>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-tab-panel>
+            </q-tab-panels>
+          </q-card>
+        </q-dialog>
         <!-- </q-menu> -->
         <LiftOffer
           class="q-mb-sm"
@@ -68,33 +236,29 @@
 import { sendApiRequest } from "../ApiAccess";
 import LiftOffer from "../components/LiftOffer";
 import QrLiftDisplay from "components/QrLiftDisplay";
+import TitleButton from "components/TitleButtonAnchor";
 
 export default {
-  components: { LiftOffer, QrLiftDisplay },
+  components: { LiftOffer, QrLiftDisplay, TitleButton },
   data() {
     return {
       openEditSort: false,
       sort: {
         label: "nach Sitzen (Standard)",
         icon: "person",
-        value: "seats"
+        val: "seats"
       },
-      offerIndexToRefresh: 1
+      offerIndexToRefresh: 1,
+      filter: {
+        friends: 0,
+        prefs: true,
+        dest: 0
+      },
+      sortFilterDialog: {
+        open: false,
+        tab: "sort"
+      }
     };
-  },
-
-  /* updated() {
-    this.recomputeView();
-  },
-
-  created() {
-    this.recomputeView();
-  }, */
-
-  watch: {
-    /* $route(to) {
-      this.recomputeView();
-    } */
   },
 
   computed: {
@@ -113,35 +277,44 @@ export default {
           label: "niedrigste Entfernung",
           caption: "Sortiert aufsteigend nach Luftlinie",
           icon: "location_on",
-          value: "distance",
+          val: "distance",
           disabled: true
         },
         {
           label: "wenigste Sitze belegt",
           caption: "Sortiert nach freien Plätzen",
-          value: "seats",
+          val: "seats",
           icon: "person"
         },
         {
           label: "Toleranz",
-          caption: "Sortiert nach der Toleranz des Fahrers",
-          value: "prefs",
+          caption:
+            "Sortiert nach der Toleranz bzw. den Präferenzen des Fahrers",
+          val: "prefs",
           icon: "insert_emoticon"
         },
         {
           label: "Zeit",
           caption: "Sortiert nach Zeit der Fahrt",
-          value: "timeDiff",
+          val: "timeDiff",
           icon: "departure_board"
         },
         {
           label: "Geringste Zeit zum Unterricht",
           caption: "Möglichst geringe Wartezeit bis Beginn der Veranstaltung",
-          value: "time",
+          val: "time",
           icon: "timer",
           disabled: true
         }
       ];
+    },
+
+    filtersActive() {
+      var active = 0;
+      Object.values(this.filter).forEach(val => {
+        if (val) active++;
+      });
+      return active;
     },
 
     getSortedOffers() {
@@ -150,7 +323,7 @@ export default {
       // here comes the sorting code
 
       if (this.sort) {
-        switch (this.sort.value) {
+        switch (this.sort.val) {
           case "distance":
             offers.sort((a, b) => {
               return a.distance - b.distance;
@@ -249,6 +422,16 @@ export default {
 
     hide() {
       this.qrLiftData = "";
+    },
+
+    openSortFilterDialog(prop) {
+      this.sortFilterDialog.open = true;
+      this.sortFilterDialog.tab = prop;
+    },
+
+    keepDialogOpen(prop, val) {
+      this.sortFilterDialog.open = true; // just to be sure
+      this[prop] = val;
     },
 
     recomputeView() {
